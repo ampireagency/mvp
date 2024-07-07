256c879c8be05748aa0bcdb98c22e8fd41129d7d
Gokula krishnan R <105733059+gokula-krishnan-r-dev@users.noreply.github.com>
2024-07-07 17:58:36 +0530

    refactor: Update landing page banner color and process steps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -181,7 +181,7 @@ export default function Home() {
 
         {/* <IconBlocks /> */}
         <div className="container mx-auto px-4 py-8">
-          <h1 className="text-3xl font-bold text-center mb-8">How its Work</h1>
+          <h1 className="text-3xl font-bold text-center mb-8">How it Work</h1>
           <ProcessSteps />
         </div>
 
--- src/components/process-steps.tsx
+++ src/components/process-steps.tsx
@@ -1,67 +1,69 @@
-// components/ProcessSteps.tsx
-import Image from "next/image";
-
 const steps = [
   {
     number: 1,
     title: "Scan Student Work",
     description:
       "It pays off: saves time overall, prevents cheating, and frees your office of old exams.",
-    image:
-      "https://ralfvanveen.com/wp-content/uploads/2021/06/Placeholder-_-Glossary.svg",
+    image: "/images/scan.jpg",
   },
   {
     number: 2,
     title: "Grade Submissions",
     description:
       "Give detailed feedback while maintaining consistency with a flexible rubric.",
-    image:
-      "https://ralfvanveen.com/wp-content/uploads/2021/06/Placeholder-_-Glossary.svg",
+    image: "/images/Grade.svg",
   },
   {
     number: 3,
     title: "Send & Export Grades",
     description:
       "Send grades to students with a click or export them to your own gradebook.",
-    image:
-      "https://ralfvanveen.com/wp-content/uploads/2021/06/Placeholder-_-Glossary.svg",
+    image: "/images/Sent.svg",
   },
   {
     number: 4,
     title: "Get Detailed Analytics",
     description:
       "Get per-question and per-rubric statistics to understand how your students are doing.",
-    image:
-      "https://ralfvanveen.com/wp-content/uploads/2021/06/Placeholder-_-Glossary.svg",
+    image: "/images/analytics.svg",
   },
 ];
 
 const ProcessSteps = () => {
   return (
-    <div className="">
+    <div className="relative">
       <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+        <svg
+          className="processSteps--desktopThroughLine"
+          preserveAspectRatio="none"
+          viewBox="0 0 100 2"
+        >
+          <line x1="0" x2="100" y1="1" y2="1"></line>
+        </svg>
         {steps.map((step) => (
-          <div
-            key={step.number}
-            className="text-center relative p-4 rounded-lg"
-          >
-            <div className="relative">
-              <div className="flex items-center justify-center">
-                <img
-                  src={step.image}
-                  alt={step.title}
-                  className="w-[200px] h-[200px] rounded-full border-2 border-green-500"
-                  width={500}
-                  height={300}
-                />
-                <div className="absolute bottom-0 left-16 bg-green-500 text-white rounded-full w-8 h-8 flex items-center justify-center">
-                  {step.number}
+          <>
+            <div
+              key={step.number}
+              className="text-center relative p-4 rounded-lg"
+            >
+              <div className="relative">
+                <div className="flex items-center justify-center">
+                  <img
+                    src={step.image}
+                    alt={step.title}
+                    className="w-[200px] bg-white h-[200px] rounded-full border-2 border-green-500"
+                    width={500}
+                    height={300}
+                  />
+                  <div className="absolute bottom-0 left-16 bg-green-500 text-white rounded-full w-8 h-8 flex items-center justify-center">
+                    {step.number}
+                  </div>
                 </div>
               </div>
+              <h3 className="mt-4 text-xl font-bold">{step.title}</h3>
+              <p className="mt-2 text-gray-600">{step.description}</p>
             </div>
-            <h3 className="mt-4 text-xl font-bold">{step.title}</h3>
-            <p className="mt-2 text-gray-600">{step.description}</p>
-          </div>
+          </>
         ))}
       </div>
     </div>
