1b9b128348a1020fb979984dd354184d38f7629c
Gokula krishnan R <105733059+gokula-krishnan-r-dev@users.noreply.github.com>
2024-07-10 14:36:36 +0530

    refactor: Update landing page layout, images, and banner color

--- src/app/(default)/dashboard/create-assesment/page.tsx
+++ src/app/(default)/dashboard/create-assesment/page.tsx
@@ -4,76 +4,81 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="lg:ps-64 w-full flex min-h-screen h-[80vh] items-center justify-center">
-      <div className="space-y-4">
-        <DatePicker />
+    <>
+      <h1 className="px-4 lg:ps-64 w-full flex text-2xl font-bold items-center justify-center py-6 text-black">
+        Create assessment
+      </h1>
+      <div className="lg:ps-64 w-full flex  h-[60vh] items-center justify-center">
         <div className="space-y-4">
-          <div className="flex rounded-lg shadow-sm">
-            <span className="px-4 inline-flex items-center min-w-fit rounded-s-md border border-e-0 border-gray-200 bg-gray-50 text-sm text-gray-500 dark:bg-neutral-700 dark:border-neutral-700 dark:text-neutral-400">
-              Subject
-            </span>
-            <input
-              type="text"
-              className="py-3 px-4 pe-11 block border w-full border-gray-200 shadow-sm rounded-e-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-            />
-          </div>
-          <div className="flex rounded-lg shadow-sm">
-            <span className="px-4 inline-flex items-center min-w-fit rounded-s-md border border-e-0 border-gray-200 bg-gray-50 text-sm text-gray-500 dark:bg-neutral-700 dark:border-neutral-700 dark:text-neutral-400">
-              Upload the answer key (optional):
-            </span>
-            <input
-              placeholder="optional"
-              type="text"
-              className="py-3 px-4 pe-11 block border w-full border-gray-200 shadow-sm rounded-e-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-            />
-          </div>
-          <div className="flex rounded-lg shadow-sm">
-            <span className="px-4 inline-flex items-center min-w-fit rounded-s-md border border-e-0 border-gray-200 bg-gray-50 text-sm text-gray-500 dark:bg-neutral-700 dark:border-neutral-700 dark:text-neutral-400">
-              Http://
-            </span>
-            <input
-              placeholder="You can share the drive folder link here"
-              type="text"
-              className="py-3 px-4 pe-11 block border w-full border-gray-200 shadow-sm rounded-e-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-            />
-          </div>
-          <div className="flex rounded-lg shadow-sm">
-            <span className="px-4 inline-flex items-center min-w-fit rounded-s-md border border-e-0 border-gray-200 bg-gray-50 text-sm text-gray-500 dark:bg-neutral-700 dark:border-neutral-700 dark:text-neutral-400">
-              Choose File
-            </span>
-            <input
-              placeholder="Upload the students answer sheets here"
-              type="file"
-              className="py-3 px-4 pe-11 block border w-full border-gray-200 shadow-sm rounded-e-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-            />
-          </div>
+          <DatePicker />
+          <div className="space-y-4">
+            <div className="flex rounded-lg shadow-sm">
+              <span className="px-4 inline-flex items-center min-w-fit rounded-s-md border border-e-0 border-gray-200 bg-gray-50 text-sm text-gray-500 dark:bg-neutral-700 dark:border-neutral-700 dark:text-neutral-400">
+                Subject
+              </span>
+              <input
+                type="text"
+                className="py-3 px-4 pe-11 block border w-full border-gray-200 shadow-sm rounded-e-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+              />
+            </div>
+            <div className="flex rounded-lg shadow-sm">
+              <span className="px-4 inline-flex items-center min-w-fit rounded-s-md border border-e-0 border-gray-200 bg-gray-50 text-sm text-gray-500 dark:bg-neutral-700 dark:border-neutral-700 dark:text-neutral-400">
+                Upload the answer key (optional):
+              </span>
+              <input
+                placeholder="optional"
+                type="text"
+                className="py-3 px-4 pe-11 block border w-full border-gray-200 shadow-sm rounded-e-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+              />
+            </div>
+            <div className="flex rounded-lg shadow-sm">
+              <span className="px-4 inline-flex items-center min-w-fit rounded-s-md border border-e-0 border-gray-200 bg-gray-50 text-sm text-gray-500 dark:bg-neutral-700 dark:border-neutral-700 dark:text-neutral-400">
+                Http://
+              </span>
+              <input
+                placeholder="You can share the drive folder link here"
+                type="text"
+                className="py-3 px-4 pe-11 block border w-full border-gray-200 shadow-sm rounded-e-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+              />
+            </div>
+            <div className="flex rounded-lg shadow-sm">
+              <span className="px-4 inline-flex items-center min-w-fit rounded-s-md border border-e-0 border-gray-200 bg-gray-50 text-sm text-gray-500 dark:bg-neutral-700 dark:border-neutral-700 dark:text-neutral-400">
+                Choose File
+              </span>
+              <input
+                placeholder="Upload the students answer sheets here"
+                type="file"
+                className="py-3 px-4 pe-11 block border w-full border-gray-200 shadow-sm rounded-e-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+              />
+            </div>
 
-          <div className="flex items-center w-full justify-center">
-            <Link
-              className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-green-500 text-white hover:bg-green-500 disabled:opacity-50 disabled:pointer-events-none"
-              href="/dashboard/list"
-            >
-              <svg
-                className="flex-shrink-0 size-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
+            <div className="flex items-center w-full justify-center">
+              <Link
+                className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-green-500 text-white hover:bg-green-500 disabled:opacity-50 disabled:pointer-events-none"
+                href="/dashboard/list"
               >
-                <path d="M5 12h14" />
-                <path d="M12 5v14" />
-              </svg>
-              Evaluate
-            </Link>
+                <svg
+                  className="flex-shrink-0 size-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={24}
+                  height={24}
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M5 12h14" />
+                  <path d="M12 5v14" />
+                </svg>
+                Evaluate
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
