07ce289918d65bef058369af7e978ab1834028a6
manoprasanth-m <65891111+manoprasanth-m@users.noreply.github.com>
2024-07-10 12:55:27 +0100

    Create table-list.tsx

---
+++ src/components/shared/table-list.tsx
@@ -0,0 +1,108 @@
+import React from "react";
+import Link from "next/link";
+
+const data = [
+  {
+    name: "Rahul",
+    age: 45,
+    address: "New York No. 1 Lake Park",
+    studentId: "1234",
+    date: "12/12/2021",
+    subject: "Mathematics",
+  },
+  {
+    name: "Ajay",
+    age: 27,
+    address: "London No. 1 Lake Park",
+    studentId: "1235",
+    date: "12/12/2021",
+    subject: "Physics",
+  },
+];
+
+const TableList = ({ isView }: any) => {
+  return (
+    <div>
+      <div className="flex flex-col">
+        <div className="-m-1.5 overflow-x-auto">
+          <div className="p-1.5 min-w-full inline-block align-middle">
+            <div className="border rounded-lg overflow-hidden dark:border-neutral-700">
+              <table className="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
+                <thead className="bg-gray-50 dark:bg-neutral-700">
+                  <tr>
+                    <th>
+                      <div className="ps-6 py-2">
+                        <label
+                          htmlFor="hs-at-with-checkboxes-1"
+                          className="flex"
+                        >
+                          <input
+                            type="checkbox"
+                            className="shrink-0 border-gray-300 rounded text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-600 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
+                            id="hs-at-with-checkboxes-1"
+                          />
+                          <span className="sr-only">Checkbox</span>
+                        </label>
+                      </div>
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-400"
+                    >
+                      Student ID
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-400"
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase dark:text-neutral-400"
+                    >
+                      Result
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200 dark:divide-neutral-700">
+                  {data.map((person, index) => (
+                    <tr key={index}>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <div className="flex items-center">
+                          <div className="flex-shrink-0">
+                            <input
+                              type="checkbox"
+                              className="shrink-0 border-gray-300 rounded text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-600 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
+                            />
+                          </div>
+                        </div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">
+                        {person.studentID}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">
+                        {person.name}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                        <Link
+                          href={!isView ? "#" : "/dashboard/view-assesment"}
+                          type="button"
+                          className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400"
+                        >
+                          {isView ? "view" : "In Progress"}
+                        </Link>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TableList;
